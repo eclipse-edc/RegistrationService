5abbb748fb406dd2fe950fab86ffe4c9927d1552
ndr_brt <andrea.bertagnolli@gmail.com>
2023-04-04 09:19:20 +0200

    fix: catalog publication (#83)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,22 +28,16 @@ dependencyResolutionManagement {
 }
 
 include(":core:registration-service")
+include(":core:registration-service-client")
 include(":core:registration-service-credential-service")
 include(":core:registration-service-onboarding-policy-verifier")
-include(":core:registration-service-client")
-
 include(":extensions:registration-service-api")
 include(":extensions:store:cosmos:participant-store-cosmos")
 include(":extensions:store:sql:participant-store-sql")
-
+include(":launcher")
+include(":registration-service-cli")
 include(":spi:registration-service-spi")
 include(":spi:registration-service-store-spi")
-
 include(":system-tests")
 include(":system-tests:launchers:participant")
-
-include(":registration-service-cli")
-
-include(":launcher")
-
-
+include(":version-catalog")
